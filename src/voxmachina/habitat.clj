51bed508b1516595f30cbf35a5a300486e9b6bc2
Ross McDonald <85923+rossajmcd@users.noreply.github.com>
2024-02-10 18:22:32 +0000

    fix formatting

--- src/voxmachina/habitat.clj
+++ src/voxmachina/habitat.clj
@@ -41,15 +41,15 @@
     [:get "/"] {:body "Welcome!" :status 200}
     [:get "/queue"] {:body {:queue (seq @q) :queue-size (count @q)} :status 200 :headers {"Content-Type" "application/edn"}}))
 
-  (defn -main [& args]
-    (let [cfg (cli/parse-opts *command-line-args* {:spec cli-options})
-          v (:habitat (read-string (slurp "version.edn")))]
-      (info {:main/start {:data (str "starting habitat v" v) :metadata {:habitat/version v}}})
-      (info {:filesystem/watch {:data (:src cfg) :metadata {:habitat/version v}}})
-      (fw/watch (:src cfg) (fn [{:keys [path] :as event}]
-                             (when (event-valid? event)
-                               (let [inf {:filesystem/event {:data event :metadata {:habitat/version v}}}]
-                                 (info inf)
-                                 (reset! q (queue q-cnt @q inf))))) {:recursive true})
-      (srv/run-server app {:port (or (:port cfg) 8080)})
-      @(promise)))
+(defn -main [& args]
+  (let [cfg (cli/parse-opts *command-line-args* {:spec cli-options})
+        v (:habitat (read-string (slurp "version.edn")))]
+    (info {:main/start {:data (str "starting habitat v" v) :metadata {:habitat/version v}}})
+    (info {:filesystem/watch {:data (:src cfg) :metadata {:habitat/version v}}})
+    (fw/watch (:src cfg) (fn [{:keys [path] :as event}]
+                           (when (event-valid? event)
+                             (let [inf {:filesystem/event {:data event :metadata {:habitat/version v}}}]
+                               (info inf)
+                               (reset! q (queue q-cnt @q inf))))) {:recursive true})
+    (srv/run-server app {:port (or (:port cfg) 8080)})
+    @(promise)))
